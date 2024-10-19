bdf40d4a0b2b57e584cfa32bd1133a56034fafa4
FatjonSelimaj <selimajfatjon16@gmail.com>
2024-10-19 17:08:05 +0200

    dfe

--- src/api/app.ts
+++ src/api/app.ts
@@ -36,12 +36,4 @@ app.use((req, res, next) => {
     res.status(404).send('Errore 404: Risorsa non trovata');
 });
 
-// Avvia il server
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
-
 export default app;
