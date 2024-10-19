f7392dda0ed74ea4bb0caeb25c5fd5690af750bb
FatjonSelimaj <selimajfatjon16@gmail.com>
2024-10-19 17:44:38 +0200

    aggiornamento

--- src/api/app.ts
+++ src/api/app.ts
@@ -17,23 +17,35 @@ const app = express();
 app.use(express.json());
 
 // Abilita CORS
-app.use(cors({ origin: true }));
+app.use(cors({
+    origin: ['https://storia-letteratura-follower.vercel.app', 'https://stori-letteratura.vercel.app'],
+    credentials: true
+}));
+
 
 // Registra le rotte con prefisso /api
-app.use('//api/articles', articleRoutes);
-app.use('//api', articleRoutes);
-app.use('//api/authors', authorRoutes);
-app.use('//api', authorRoutes);
-app.use('//api/works', workRoutes);  // Registra la rotta /api/works
-app.use('//api', workRoutes);  // Registra la rotta /api/works
-app.use('//api/literatures', literatureRoutes);
-app.use('//api', literatureRoutes);
-app.use('//api/history-sections', historySectionRoutes);
-app.use('//api', historySectionRoutes);
+app.use('/api/articles', articleRoutes);
+app.use('/api', articleRoutes);
+app.use('/api/authors', authorRoutes);
+app.use('/api', authorRoutes);
+app.use('/api/works', workRoutes);  // Registra la rotta /api/works
+app.use('/api', workRoutes);  // Registra la rotta /api/works
+app.use('/api/literatures', literatureRoutes);
+app.use('/api', literatureRoutes);
+app.use('/api/history-sections', historySectionRoutes);
+app.use('/api', historySectionRoutes);
 
 // Gestisci le rotte non trovate
-app.use((req, res, next) => {
+app.use((_req, res, _next) => {
     res.status(404).send('Errore 404: Risorsa non trovata');
 });
+
+// Avvia il server
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
 export default app;
