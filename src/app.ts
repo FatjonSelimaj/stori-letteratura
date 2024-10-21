ce24b8844add4f0b4dbeff0613a74cd3099c386a
FatjonSelimaj <selimajfatjon16@gmail.com>
2024-10-21 21:07:30 +0200

    dk

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 // Importa le rotte
 import articleRoutes from './routes/articleRoutes';
 import authorRoutes from './routes/authorRoutes';
-import workRoutes from './routes/workRoutes';  // Importa la rotta per "works"
+import workRoutes from './routes/workRoutes';
 import literatureRoutes from './routes/literatureRoutes';
 import historySectionRoutes from './routes/historySectionRoutes';
 
@@ -21,23 +21,15 @@ app.use(cors({ origin: true }));
 
 // Registra le rotte con prefisso /api
 app.use('/api/articles', articleRoutes);
-app.use('/api', articleRoutes);
 app.use('/api/authors', authorRoutes);
-app.use('/api/', authorRoutes);
-app.use('/api/works', workRoutes); 
-app.use('/api', workRoutes); 
+app.use('/api/works', workRoutes);
 app.use('/api/literatures', literatureRoutes);
-app.use('/api/', literatureRoutes);
 app.use('/api/history-sections', historySectionRoutes);
-app.use('/api', historySectionRoutes);
 
 // Gestisci le rotte non trovate
-app.use((req, res, next) => {
+app.use((_req, res, _next) => {
     res.status(404).send('Errore 404: Risorsa non trovata');
 });
 
-// Avvia il server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Esporta l'app per Vercel
+export default app;
