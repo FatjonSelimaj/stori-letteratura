05afe5883dc5985c02e0cadd8a886517d206d1e6
FatjonSelimaj <selimajfatjon16@gmail.com>
2024-10-23 20:14:51 +0200

    https

--- src/api/app.ts
+++ src/api/app.ts
@@ -13,13 +13,21 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: ['https://storia-letteratura-follower.vercel.app'], // Dominio frontend autorizzato
-  credentials: true,  // Permetti invio di cookie o credenziali
-}));
-
-// Middleware per il parsing del JSON
+// Middleware per il parsing del JSON nel corpo delle richieste
 app.use(express.json());
+
+// Configurazione CORS: accetta richieste solo da origini specifiche in produzione
+const allowedOrigins = ['https://stori-letteratura.vercel.app/', 'https://storia-letteratura-admin-frontend.vercel.app/', 'https://storia-letteratura-follower.vercel.app/'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Se necessario inviare cookie o credenziali
+}));
 
 // Registra le rotte con prefisso /api
 app.use('/api/articles', articleRoutes);
@@ -28,9 +36,15 @@ app.use('/api/works', workRoutes);
 app.use('/api/literatures', literatureRoutes);
 app.use('/api/history-sections', historySectionRoutes);
 
+// Gestisci le rotte non trovate
 app.use((req, res, next) => {
     res.status(404).send('Errore 404: Risorsa non trovata');
-  });
-  
-  // Esporta l'app per Vercel
-  export default app;
+});
+
+// Avvia il server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
